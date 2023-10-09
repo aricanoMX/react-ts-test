e81ef6c0870b1b4ff71ae3ff582ff8e88d4c2d6a
Andrés Ricaño <aricanomx@gmail.com>
2023-10-09 14:15:44 -0600

    [Feature]
    message: lazy components was added

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, NavLink, Navigate, Route, Routes } from 'react-router-dom';
 import logo from '../assets/react.svg';
+import { LazyPage1, LazyPage2, LazyPage3 } from '../01-lazyload/pages';
+
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -8,27 +10,28 @@ export const Navigation = () => {
           <img src={logo} alt='React Logo' />
           <ul>
             <li>
-              <NavLink to='/' className={({ isActive }) => (isActive ? 'nav-active' : '')}>
+              <NavLink to='/lazy1' className={({ isActive }) => (isActive ? 'nav-active' : '')}>
                 Home
               </NavLink>
             </li>
             <li>
-              <NavLink to='/about' className={({ isActive }) => (isActive ? 'nav-active' : '')}>
+              <NavLink to='/lazy2' className={({ isActive }) => (isActive ? 'nav-active' : '')}>
                 About
               </NavLink>
             </li>
             <li>
-              <NavLink to='/users' className={({ isActive }) => (isActive ? 'nav-active' : '')}>
+              <NavLink to='/lazy3' className={({ isActive }) => (isActive ? 'nav-active' : '')}>
                 Users
               </NavLink>
             </li>
           </ul>
         </nav>
+
         <Routes>
-          <Route path='/' element={<h1>Home</h1>} />
-          <Route path='/about' element={<h1>About</h1>} />
-          <Route path='/users' element={<h1>Users</h1>} />
-          <Route path='*' element={<Navigate to='/' replace />} />
+          <Route path='/lazy1' element={<LazyPage1 />} />
+          <Route path='/lazy2' element={<LazyPage2 />} />
+          <Route path='/lazy3' element={<LazyPage3 />} />
+          <Route path='*' element={<Navigate to='/lazy1' replace />} />
         </Routes>
       </div>
     </BrowserRouter>
